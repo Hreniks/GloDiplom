b05d8525981f19c0b6213536f2b9f2d132c70e58
Hreniks <sbigor09@gmail.com>
2020-11-26 00:08:41 +0300

    added showing arrow

--- src/modules/arrow.js
+++ src/modules/arrow.js
@@ -1,5 +1,14 @@
 const arrow = () => {
-
-}
+    const upBtn = document.querySelector('#totop');
+   
+    window.addEventListener('scroll', () =>{
+        console.log('pageyoffset',pageYOffset);
+        console.log('clientheight',document.documentElement.clientHeight);
+        if (pageYOffset > 750)
+        upBtn.style.display = 'block';
+        else
+        upBtn.style.display = 'none';
+    });
+};
 
 export default arrow;
